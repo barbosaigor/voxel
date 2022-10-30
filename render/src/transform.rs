bd92d9d79f5e1a5846629dc426837d48f3035d26
barbosaigor <igor.barbosa1920@gmail.com>
2022-10-30 14:52:11 -0300

    chore: remove commends

--- render/src/transform.rs
+++ render/src/transform.rs
@@ -13,52 +13,6 @@ impl TransformMatrix {
             array_stride: mem::size_of::<TransformMatrix>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Instance,
             attributes: &[
-                // wgpu::VertexAttribute {
-                //     offset: 0,
-                //     shader_location: 2,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // wgpu::VertexAttribute {
-                //     offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
-                //     shader_location: 3,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // wgpu::VertexAttribute {
-                //     offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
-                //     shader_location: 4,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // wgpu::VertexAttribute {
-                //     offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
-                //     shader_location: 5,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // --
-                // wgpu::VertexAttribute {
-                //     // transform 1
-                //     offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
-                //     shader_location: 2,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // wgpu::VertexAttribute {
-                //     // transform 2
-                //     offset: mem::size_of::<[f32; 13]>() as wgpu::BufferAddress,
-                //     shader_location: 3,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // wgpu::VertexAttribute {
-                //     // transform 3
-                //     offset: mem::size_of::<[f32; 18]>() as wgpu::BufferAddress,
-                //     shader_location: 4,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // wgpu::VertexAttribute {
-                //     // transform 4
-                //     offset: mem::size_of::<[f32; 23]>() as wgpu::BufferAddress,
-                //     shader_location: 5,
-                //     format: wgpu::VertexFormat::Float32x4,
-                // },
-                // --
                 wgpu::VertexAttribute {
                     // transform 1
                     offset: 0,
