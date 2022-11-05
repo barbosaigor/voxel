c82e5c55a1a03a2b714a4f2057284b081d0bf876
barbosaigor <igor.barbosa1920@gmail.com>
2022-11-05 14:40:08 -0300

    chore: update voxel specs example

--- voxel/examples/ecs2/main.rs
+++ voxel/examples/ecs2/main.rs
@@ -26,6 +26,26 @@ impl<'a> System<'a> for HelloWorld {
     }
 }
 
+struct UpdatePos;
+
+impl<'a> System<'a> for UpdatePos {
+    type SystemData = (
+        Read<'a, DeltaTime>,
+        ReadStorage<'a, Velocity>, 
+        WriteStorage<'a, Position>,
+    );
+
+    fn run(&mut self, (dt, vel, mut pos): Self::SystemData) {
+
+        for (vel, pos) in (&vel, &mut pos).join() {
+            pos.x += vel.x * dt.0;
+            pos.y += vel.y * dt.0;
+        }
+    }
+}
+
+#[derive(Default)]
+struct DeltaTime(f32);
 
 fn main() {
     let mut world = World::new();
@@ -33,12 +53,29 @@ fn main() {
     world.register::<Position>();
     world.register::<Velocity>();
 
-    let _ = world.create_entity().with(Position { x: 4.0, y: 7.0 }).build();
-    let _ = world.create_entity().with(Position { x: 10.0, y: 11.0 }).build();
+    world.insert(DeltaTime(0.0));
 
-    let mut hello_world = HelloWorld;
-    hello_world.run_now(&world);
-    let _ = world.create_entity().with(Position { x: 10.0, y: 12.0 }).build();
+    let _ = world
+        .create_entity()
+        .with(Position { x: 4.0, y: 7.0 })
+        .with(Velocity { x: 0.5, y: 0.0 })
+        .build();
+    let _ = world
+        .create_entity()
+        .with(Position { x: 10.0, y: 11.0 })
+        .with(Velocity { x: 0.5, y: 0.0 })
+        .build();
+
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(HelloWorld, "hello_world", &[])
+        .with(UpdatePos, "update_pos", &["hello_world"])
+        .with(HelloWorld, "hello_updated", &["update_pos"])
+        .build();
+
+    let mut dt = world.write_resource::<DeltaTime>();
+    *dt = DeltaTime(0.05);
+    // drop(dt);
+
+    dispatcher.dispatch(&mut world);
     world.maintain();
-    hello_world.run_now(&world);
 }
